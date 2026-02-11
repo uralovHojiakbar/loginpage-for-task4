9715b641ea17c342a7ce5b670c0e8aa70c2551c5
hojiakbaruralov293 <hojiakbaruralov293@gmail.com>
2026-02-11 11:14:27 +0500

    added Dockerfile

--- loginpage/Program.cs
+++ loginpage/Program.cs
@@ -6,6 +6,12 @@ using loginpage.Middlewares;
 using loginpage.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+var port = Environment.GetEnvironmentVariable("PORT");
+if (!string.IsNullOrWhiteSpace(port))
+{
+    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+}
+
 
 builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
